5d686536b27330283943585eb81a02033dd10025
Guillermo Cirer <guille.mca@hotmail.com>
2017-05-15 16:38:01 +0200

    reestructurado codigo de la vista /signup

--- middleware/Utils.py
+++ middleware/Utils.py
@@ -5,8 +5,8 @@ class Utils():
 	
 	@staticmethod
 	def getDevice():
-		device = request.user_agent.platform
-		if device == 'windows' or device == 'linux' or device == 'macos':
-			return 'horizontal'
-		else:
-			return 'vertical'
+		# device = request.user_agent.platform
+		# if device == 'windows' or device == 'linux' or device == 'macos':
+		# 	return 'horizontal'
+		# else:
+		return 'vertical'
